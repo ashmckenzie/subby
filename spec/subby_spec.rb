d41359df1af00f7b7f32665f73ae754c48e6aac7
Ash McKenzie <ash@greenworm.com.au>
2013-02-25 07:53:54 +1100

    getting there..

--- spec/subby_spec.rb
+++ spec/subby_spec.rb
@@ -13,91 +13,130 @@ describe Subby do
     #   end
     # end
 
-    context 'with empty input and no block' do
+    context 'without command' do
 
-      it 'raises an exception' do
-        expect { Subby.setup }.to raise_error(ArgumentError)
-      end
-    end
+      context 'with empty input and no block' do
 
-    context 'with no input defined' do
-
-      subject do
-        Subby.setup input do
-          switch :switch, 'Optional switch'
+        it 'raises an exception' do
+          expect { Subby.setup }.to raise_error(ArgumentError)
         end
       end
 
-      let(:input) { %w{} }
-
-      it 'has no options' do
-        subject.command.should be_nil
-      end
-    end
-
-    context 'with one switch configured' do
-
-      subject do
-        Subby.setup input do
-          switch :switch, 'Optional switch'
-        end
-      end
-
-      context 'with input defined as --switch' do
-        let(:input) { %w{ --switch } }
-
-        it 'has one option of type Subby::Switch' do
-          subject.options.count.should == 1
-          subject.options[:switch].should be_instance_of(Subby::Switch)
-        end
-      end
-
-    end
-
-    context 'with one optional parameter configured' do
-
-
-      context 'without format defined' do
+      context 'with no input defined' do
 
         subject do
           Subby.setup input do
-            parameter :parameter, 'Optional parameter'
+            switch :switch, 'Optional switch'
           end
         end
 
-        context 'with no input defined' do
-          let(:input) { %w{} }
+        let(:input) { %w{} }
 
-          it 'has no options' do
-            subject.command.should be_nil
-          end
+        it 'has no options' do
+          subject.command.should be_nil
         end
-
-        context 'with input defined as --parameter abc' do
-          let(:input) { %w{ --parameter abc } }
-
-          it 'raises an Error::InvalidParameter exception' do
-            expect { subject }.to raise_error(Subby::Error::InvalidParameter)
-          end
-        end
-
       end
 
-      context 'with format defined' do
+      context 'with one switch configured' do
 
         subject do
           Subby.setup input do
-            parameter :parameter, 'Optional parameter', format: /\w+/
+            switch :switch, 'Optional switch'
           end
         end
 
-        context 'with input defined as --parameter abc' do
-          let(:input) { %w{ --parameter abc } }
+        context 'with input defined as --switch' do
+          let(:input) { %w{ --switch } }
 
           it 'has one option of type Subby::Switch' do
             subject.options.count.should == 1
+            subject.options[:switch].should be_instance_of(Subby::Switch)
+          end
+        end
+      end
+
+      context 'with one optional parameter configured' do
+
+        context 'without format defined' do
+
+          subject do
+            Subby.setup input do
+              parameter :parameter, 'Optional parameter'
+            end
+          end
+
+          context 'with no input defined' do
+            let(:input) { %w{} }
+
+            it 'has no options' do
+              subject.command.should be_nil
+            end
+          end
+
+          context 'with input defined as --parameter abc' do
+            let(:input) { %w{ --parameter abc } }
+
+            it 'raises an Error::InvalidParameter exception' do
+              expect { subject }.to raise_error(Subby::Error::InvalidParameter)
+            end
+          end
+
+        end
+
+        context 'with format defined' do
+
+          subject do
+            Subby.setup input do
+              parameter :parameter, 'Optional parameter', format: /\w+/
+            end
+          end
+
+          context 'with input defined as --parameter abc' do
+            let(:input) { %w{ --parameter abc } }
+
+            it 'has one option of type Subby::Parameter' do
+              subject.options.count.should == 1
+              subject.options[:parameter].should be_instance_of(Subby::Parameter)
+            end
+          end
+        end
+      end
+
+      context 'with one switch and mandatory parameter configured' do
+
+        subject do
+          Subby.setup input do
+            switch :switch, 'Switch'
+            parameter :parameter, 'Optional parameter', format: /\w+/, required: true
+          end
+        end
+
+        context 'with input defined as --switch --parameter abc' do
+          let(:input) { %w{ --switch --parameter abc } }
+
+          it 'has one option of type Subby::Switch' do
+            subject.options.count.should == 2
             subject.options[:parameter].should be_instance_of(Subby::Parameter)
           end
+        end
+      end
+    end
+
+    context 'with command' do
+
+      context 'a single command' do
+
+        let(:input) { %w{ command } }
+
+        subject do
+          Subby.setup input do
+            command :command do
+            end
+          end
+        end
+
+        it 'has one command of type Subby::Command' do
+          subject.command.should be_instance_of(Subby::Command)
         end
       end
     end
